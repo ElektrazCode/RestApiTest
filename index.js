6aef26a90364771be49b96ad0a4bd5c77903d809
ElektrazCode <ElektrazCode@gmail.com>
2022-06-16 17:31:12 -0400

    Added Create Feature

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ app.get('/', (request, response) => {
     response.end('<h1>Hello World!</h1>');
 });
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id);
     const note = notes.find(note => note.id === id);
@@ -50,9 +54,30 @@ app.delete('/api/notes/:id', (request, response)=>{
     response.status(204).end();
 })
 
+const generateId = () =>{
+  const maxId = notes.length > 0 ?
+                Math.max(... notes.map(note=>note.id)) : 0;
+  return maxId + 1;
+}
+
 app.post('/api/notes', (request, response)=> {
-    const note = request.body;
-    console.log(note);
+    const body = request.body;
+
+    if(!body.content){
+      return response.status(400).json({
+        error: 'Content Missing'
+      })
+    }
+
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId()
+    }
+
+    notes = notes.concat(note);
+
     response.json(note);
 })
 
